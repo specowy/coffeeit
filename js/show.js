4812c554d305113a1915af97317dd9b075500145
specowy <57944273+specowy@users.noreply.github.com>
2021-04-15 13:38:01 +0200

    Add files via upload

---
+++ js/show.js
@@ -0,0 +1,35 @@
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  var $navbarBurgers = Array.prototype.slice.call(
+    document.querySelectorAll(".navbar-burger"),
+    0
+  );
+
+  if ($navbarBurgers.length > 0) {
+   
+    $navbarBurgers.forEach(function($el) {
+      $el.addEventListener("click", function() {
+        var target = $el.dataset.target;
+        var $target = document.getElementById(target);
+        $el.classList.toggle("is-active");
+        $target.classList.toggle("is-active");
+     
+      });
+    });
+  }
+});
+
+
+
+//Scrolling
+$(document).on("click", 'a[href^="#"]', function(event) {
+  event.preventDefault();
+  $("html, body").animate(
+    {
+      scrollTop: $($.attr(this, "href")).offset().top
+    },
+    500
+  );
+});
+
